1485d985b69d98576288509c546b73d67010a4d2
irene <irene@25sprout.com>
2019-10-13 17:59:20 +0800

    fix: add home page animation

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { hot } from 'react-hot-loader/root';
 import { useTranslation } from 'react-i18next';
+import { animated, useSpring, useChain, config } from 'react-spring';
 
 import { useReactRouter } from 'models/route';
 import { useLanguage } from 'models/i18n';
@@ -23,14 +24,51 @@ const Home = () => {
 		}
 	}, [keyPress]);
 
+	const titleRef = useRef();
+	const titleProps = useSpring({
+		ref: titleRef,
+		from: { opacity: 0, letterSpacing: 0 },
+		to: async next => {
+			await next({ opacity: 1, letterSpacing: 0 });
+			await next({ opacity: 1, letterSpacing: 10 });
+		},
+		config: config.molasses,
+	});
+
+	const descriptioneRef = useRef();
+	const descriptionProps = useSpring({
+		ref: descriptioneRef,
+		from: { opacity: 0 },
+		to: { opacity: 1 },
+		config: config.stiff,
+	});
+
+	const hintRef = useRef();
+	const hintProps = useSpring({
+		ref: hintRef,
+		from: { opacity: 0 },
+		to: { opacity: 0.5 },
+		config: config.stiff,
+	});
+
+	useChain([titleRef, descriptioneRef, hintRef], [2, 6, 7]);
+
 	return (
 		<div className={styles.home}>
 			<div className={styles.container}>
-				<div className={styles.title}>{t('title')}</div>
-				<div className={styles.description}>{t('text1')}</div>
-				<div className={styles.description}>{t('text2')}</div>
+				<animated.div className={styles.title} style={titleProps}>
+					{t('title')}
+				</animated.div>
+				<animated.div className={styles.description} style={descriptionProps}>
+					{t('text1')}
+				</animated.div>
+				<animated.div className={styles.description} style={descriptionProps}>
+					{t('text2')}
+				</animated.div>
 			</div>
-			<div className={styles.hint}>{t('press')}</div>
+			<animated.div className={styles.hint} style={hintProps}>
+				{t('press')}
+			</animated.div>
 			<Waves className={styles.waves} />
 		</div>
 	);
