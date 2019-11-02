91f32a47d0035115f252ae1658218f26c5c78bde
irene <irene277299@gmail.com>
2019-11-02 13:19:39 +0800

    fix: add slider into page

--- src/routes/Portfolio/component.js
+++ src/routes/Portfolio/component.js
@@ -4,8 +4,12 @@ import classnames from 'classnames';
 import { useTranslation } from 'react-i18next';
 
 import PageScrollContainer from 'components/organisms/PageScrollContainer';
+import Slider from 'components/molecules/Slider';
+import Card01 from 'components/molecules/Card01';
 
 import Page from 'layouts/Page';
+
+import worksData from 'util/data/works/ui&ux';
 
 import styles from './index.css';
 
@@ -21,8 +25,29 @@ const Portfolio = ({ className }) => {
 	return (
 		<div className={classnames(styles.portfolio, className)}>
 			<PageScrollContainer anchors={anchors}>
+				<Section>
+					<Slider
+						slides={worksData}
+						SlideComponent={Card01}
+						config={{
+							breakpoints: {
+								600: {
+									slidesPerView: 2,
+									spaceBetween: 24,
+								},
+								900: {
+									slidesPerView: 3,
+									spaceBetween: 48,
+								},
+								1200: {
+									slidesPerView: 4,
+									spaceBetween: 64,
+								},
+							},
+						}}
+					/>
+				</Section>
 				<Section>{t('page1')}</Section>
-				<Section>test 2</Section>
 				<Section>test 3</Section>
 				<Section>test 4</Section>
 			</PageScrollContainer>
---
+++ src/util/data/works/ui&ux.js
@@ -0,0 +1,7 @@
+export default [
+	{ id: 1, title: 'UI/UX 1', description: 'Task', photo: 'https://fakeimg.pl/300/?text=01' },
+	{ id: 2, title: 'UI/UX 2', description: 'Task', photo: 'https://fakeimg.pl/300/?text=02' },
+	{ id: 3, title: 'UI/UX 3', description: 'Task', photo: 'https://fakeimg.pl/300/?text=03' },
+	{ id: 4, title: 'UI/UX 4', description: 'Task', photo: 'https://fakeimg.pl/300/?text=04' },
+	{ id: 5, title: 'UI/UX 5', description: 'Task', photo: 'https://fakeimg.pl/300/?text=05' },
+];
